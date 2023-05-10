2e6890b948c2bc8ac0408dc8c5f49aed107ca8b0
Willian <willian.m.sousa@gmail.com>
2023-05-10 19:34:37 -0300

    Sevices component

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaTeeth, FaGlobe, FaCog, FaSquare, FaSquareFull, FaSquarespace, FaStethoscope, FaHeartbeat, FaStar, FaMedal, FaPager, FaBook } from 'react-icons/fa';
 import Image from "next/image";
-import ProdutoImg from '../public/assets/pr-img-1.png'
+import ServiceImg from '../public/assets/servicos-img.png'
 
 const services = () => {
   return (
@@ -30,16 +30,74 @@ const services = () => {
       <section className="pt-20 pb-48 bg-gradient-to-r from-[#2A94D7] to-[#3678a1] rounded-xl p-10">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap justify-center text-center mb-24">
-            <div className="w-full lg:w-6/12 px-4">
-            <h3 className="text-white text-4xl font-bold uppercase underline">Serviços</h3>
-              <p className="text-white font-thin leading-relaxed m-4 text-blueGray-500">
-                Ao longo desses 28 anos de história construimos um parceirias sólidas e um produto de qualidade que já faz parte do dia-a-dia de nossos clientes
-              </p>
-              <div className='justify-center w-full h-full'>
-
-
+            <div>
+              <div>
+                <h3 className='text-red-400 underline p-8'>SERVIÇOS</h3>
+              </div>
+              <div className='lg:flex display-flex'>
+                <div>
+                  <h2 className=' text-white py-6'>Área do Cliente</h2>
+                  <p className='pt-2'>Nr 01 Ordem de Serviço, Nr 05 Cipa (Indicado pelo Empregador) Nr 06 Epis, Nr 33 (Espaço Confinado ),Nr-10 item 10.2.1 / Gerenciamento de Risco, Nr 35 (Trabalho em Altura).</p>
+                </div>
+                <div>
+                  <Image
+                    src={ServiceImg}
+                    alt="imagem do produto"
+                    width={400}
+                    height={400}
+                    className="mx-auto block lg:w-full lg:h-auto" 
+                    
+                  />
+                </div>
+              </div>
+              <div className='lg:flex lg:flex-row-reverse p-5 lg:space-y-8'>
+                <div>
+                  <h2 className=' text-white py-6'>Calandras</h2>
+                  <p className='pt-2'>Máquinas que tem o papel de curvar ou desempenar chapas de aço e metal.</p>
+                </div>
+                <div>
+                  <Image
+                    src={ServiceImg}
+                    alt="imagem do produto"
+                    width={400}
+                    height={400}
+                    className="mx-auto block lg:w-full lg:h-auto" 
+                    
+                  />
+                </div>
+              </div>
+              <div className='lg:flex display-flex'>
+                <div>
+                  <h2 className=' text-white py-6'>Combate a Incendio</h2>
+                  <p className='pt-2'>Projeto e montagem de tubulações, hidrantes, detectores de fumaça, luz de emergência, sprinkler, para-raios de combate a incêndio para lojas e industrias.</p>
+                </div>
+                <div>
+                  <Image
+                    src={ServiceImg}
+                    alt="imagem do produto"
+                    width={400}
+                    height={400}
+                    className="mx-auto block lg:w-full lg:h-auto" 
+                  />
+                </div>
+              </div>
+              <div className='lg:flex lg:flex-row-reverse'>
+                <div>
+                  <h2 className=' text-white py-6'>Eletricista de Manutenção e Montagem</h2>
+                  <p className='pt-2'>A VFX Montagens Industriais possui profissionais na área de elétrica, manutenção e montagens com certificados e treinamentos NR10 - EPI's e ferramentas adequadas para função.</p>
+                </div>
+                <div>
+                  <Image
+                    src={ServiceImg}
+                    alt="imagem do produto"
+                    width={400}
+                    height={400}
+                    className="mx-auto block lg:w-full lg:h-auto" 
+                  />
+                </div>
               </div>
             </div>
+
           </div>
 
         </div>
